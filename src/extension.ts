391cbd2b60e2e6a2a7e6139a2e48e9ea3c9a86f4
Nicolas <nicolas.trossat@gmail.com>
2024-05-03 22:56:45 +0200

    First inline AI completion

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+type ApiResponse = { response: string };
+
+
 export function activate(context: vscode.ExtensionContext) {
+	const provider = {
+        provideInlineCompletionItems: async (document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext, token: vscode.CancellationToken) => {
+            
+			try {
+				const response = await fetch('http://localhost:11434/api/generate', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ 
+						model: "llama3", 
+						prompt: document.getText(),
+						stream: false
+					}),
+				});
+			 
+				const json = await response.json() as ApiResponse;
+				const AIResponse = json.response;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "robinai" is now active!');
+				const completionItems: vscode.InlineCompletionItem[] = [];
+				const completionRange = new vscode.Range(position, position.translate(0, AIResponse.length));
+				completionItems.push({
+					insertText: AIResponse,
+					range: completionRange
+				});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('robinai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RobinAI!');
-	});
+				return completionItems;
+			} catch (err) {
+				console.error('Error while calling AI API:', err);
+			}
+        }
+    };
 
-	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
