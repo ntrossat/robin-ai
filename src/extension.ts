effb749c57b6d1e960f8d6860e5e5867d64d9e9f
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-05-04 22:55:51 +0200

    Trigger autocompletion after 1s

--- src/extension.ts
+++ src/extension.ts
@@ -7,40 +7,43 @@ export function activate(context: vscode.ExtensionContext) {
 	const provider = {
         provideInlineCompletionItems: async (document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext, token: vscode.CancellationToken) => {
             
-			if (position.line <= 0) {
+			// Wait & see before requesting the AI...
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			if (token.isCancellationRequested) {
 				return;
 			}
 
+			// Define empty result
 			const result: vscode.InlineCompletionList = {
 				items: [],
 			};
-
+			
 			try {
+				const prompt = document.getText()
 				const response = await fetch('http://localhost:11434/api/generate', {
 					method: 'POST',
 					headers: { 'Content-Type': 'application/json' },
 					body: JSON.stringify({ 
 						model: "llama3", 
-						prompt: document.getText(new vscode.Range(new vscode.Position(0, 0), position)),
+						prompt: prompt,
 						stream: false
 					}),
 				});
-			 
+				
 				const json = await response.json() as ApiResponse;
-				const AIResponse = json.response;
+				const prediction = json.response;
 
-				const completionRange = new vscode.Range(position, position.translate(0, AIResponse.length));
+				const completionRange = new vscode.Range(position, position.translate(0, prediction.length));
 				result.items.push({
-					insertText: AIResponse,
+					insertText: prediction,
 					range: completionRange
 				});
 
-				return result;
 			} catch (err) {
 				console.error('Error while calling AI API:', err);
 			}
 
-			return;
+			return result;
         }
     };
 
