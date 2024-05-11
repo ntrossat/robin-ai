2f14f66158ffe2fa71560ec4c67a4fddb92d4878
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-05-11 21:05:29 +0200

    Add options in request

--- src/extension.ts
+++ src/extension.ts
@@ -24,22 +24,31 @@ export function activate(context: vscode.ExtensionContext) {
 
 				const prefixRange = new vscode.Range(new vscode.Position(0, 0), position)
 				const prefix = document.getText(prefixRange)
-				const suffixRange = new vscode.Range(position, new vscode.Position(document.lineCount, 0), )
+
+				const suffixRange = new vscode.Range(position, new vscode.Position(document.lineCount, 0))
+
 				const suffix = document.getText(suffixRange)
 
 				const prompt = `<|fim_prefix|>${prefix}<|fim_suffix|>${suffix}<|fim_middle|>`
+
 				const response = await fetch('http://localhost:11434/api/generate', {
 					method: 'POST',
 					headers: { 'Content-Type': 'application/json' },
 					body: JSON.stringify({ 
 						model: "codegemma:code", 
 						prompt: prompt,
+						options: {
+							"stop": ["\n", "<|file_separator|>"],
+							"temperature": 0.5,
+							"num_predict": 64,
+							"repetition_penalty": 1.1
+						},
 						stream: false
 					}),
 				});
 
-				const json = await response.json() as ApiResponse;
-				const prediction = json.response;
+				const json_response = await response.json() as ApiResponse;
+				const prediction = json_response.response.replace(/\\n/g, "\n");
 
 				const endTime = performance.now();
 				console.log('PREDICTION:', prediction);
