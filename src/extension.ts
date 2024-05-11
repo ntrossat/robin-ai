aa2dab7d6b2bd227a14b681d6af38487fe8bd30f
Nicolas Trossat <nicolas.trossat@gmail.com>
2024-05-11 18:18:14 +0200

    Standalone Lite

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
         provideInlineCompletionItems: async (document: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext, token: vscode.CancellationToken) => {
             
 			// Wait & see before requesting the AI...
-			await new Promise(resolve => setTimeout(resolve, 1000));
+			await new Promise(resolve => setTimeout(resolve, 500));
 			if (token.isCancellationRequested) {
 				return;
 			}
@@ -19,19 +19,31 @@ export function activate(context: vscode.ExtensionContext) {
 			};
 			
 			try {
-				const prompt = document.getText()
+				const startTime = performance.now();
+				console.log("REQUEST")
+
+				const prefixRange = new vscode.Range(new vscode.Position(0, 0), position)
+				const prefix = document.getText(prefixRange)
+				const suffixRange = new vscode.Range(position, new vscode.Position(document.lineCount, 0), )
+				const suffix = document.getText(suffixRange)
+
+				const prompt = `<|fim_prefix|>${prefix}<|fim_suffix|>${suffix}<|fim_middle|>`
 				const response = await fetch('http://localhost:11434/api/generate', {
 					method: 'POST',
 					headers: { 'Content-Type': 'application/json' },
 					body: JSON.stringify({ 
-						model: "llama3", 
+						model: "codegemma:code", 
 						prompt: prompt,
 						stream: false
 					}),
 				});
-				
+
 				const json = await response.json() as ApiResponse;
 				const prediction = json.response;
+
+				const endTime = performance.now();
+				console.log('PREDICTION:', prediction);
+				console.log(`Elapsed time: ${(endTime - startTime)} ms`);
 
 				const completionRange = new vscode.Range(position, position.translate(0, prediction.length));
 				result.items.push({
